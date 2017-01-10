73a9c0b71530f00b5ed1ce3a7c9f18f69c63e613
Mason <q791862937@126.com>
2017-01-10 20:09:19 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,10 @@ class GroupsController < ApplicationController
     @group = Group.all
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def new
     @group = Group.new
   end
@@ -12,6 +16,10 @@ class GroupsController < ApplicationController
     @group.save
 
       redirect_to groups_path
+  end
+
+  def destory
+    
   end
 
   private
