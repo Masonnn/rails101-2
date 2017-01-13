f651f2876ce697ae7e3fe9021c7892a8661f3b22
Mason <q791862937@126.com>
2017-01-13 21:26:52 +0800

    dup

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109154723) do
-
-  create_table "groups", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "author"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
